35bd5f8fd6400e6d00e33b836413a8eefea8d6ff
Ondratra <ondratra@ondratra.eu>
2016-08-24 21:13:04 +0200

    url rules support

--- src/extension.js
+++ src/extension.js
@@ -21,6 +21,11 @@ export default class ChromeExtension extends Singleton {
 
         this.resetRequestState();
         this.tabObserver = TabsObserver.instance;
+
+        // pairs of [fromUrlRegexp, toUrlRegexp]
+        this.rules = [
+            ['.*://.*/.*', '.*://.*/.*'] // from "everywhere" to "everywhere"
+        ];
     }
 
     resetRequestState() {
@@ -46,77 +51,85 @@ export default class ChromeExtension extends Singleton {
         chrome.webRequest.onBeforeSendHeaders.addListener((arg) => {return this.sendRequest(arg)}, settings, ["blocking", "requestHeaders"]);
     }
 
-    uninstall() {
-        chrome.webRequest.onHeadersReceived.removeListener(this.recieveResponse);
-        chrome.webRequest.onBeforeSendHeaders.removeListener(this.sendRequest);
-    }
+    someRuleApply(fromUrl, toUrl) {
+        for (let [fromUrlRegexp, toUrlRegexp] of this.rules) {
+            let tmpFrom = new RegExp(fromUrlRegexp);
+            let tmpTo = new RegExp(toUrlRegexp);
 
-    sendRequest(requestDetails) {
-        let originHeaderExists = false;
-        let ruleToEnsure = {
-            name: "Origin",
-            value: "http://domainMeantTo.pass/"
-        };
-
-        for (let headerRow of requestDetails.requestHeaders) {
-            if (headerRow.name.toLowerCase() === ruleToEnsure.name.toLowerCase()) {
-                originHeaderExists = true;
-                headerRow.value = ruleToEnsure.value;
-                //break;
-            }
-
-            if (headerRow.name.toLowerCase() === headers.AccessControlRequestHeaders.toLowerCase()) {
-                this.accessControlPresent = headerRow.value;
+            if (tmpFrom.test(fromUrl) && tmpTo.test(toUrl)) {
+                return true;
             }
         }
 
-        if (!originHeaderExists) {
-            requestDetails.requestHeaders.push(ruleToEnsure);
+        return false;
+    }
+
+    sendRequest(requestDetails) {
+        this.sentFromUrl = this.tabObserver.getTabUrl(requestDetails.tabId);
+
+        if (this.someRuleApply(this.sentFromUrl, requestDetails.url)) {
+            let originHeaderExists = false;
+            let ruleToEnsure = {
+                name: "Origin",
+                value: "http://domainMeantTo.pass/"
+            };
+
+            for (let headerRow of requestDetails.requestHeaders) {
+                if (headerRow.name.toLowerCase() === ruleToEnsure.name.toLowerCase()) {
+                    originHeaderExists = true;
+                    headerRow.value = ruleToEnsure.value;
+                }
+
+                if (headerRow.name.toLowerCase() === headers.AccessControlRequestHeaders.toLowerCase()) {
+                    this.accessControlPresent = headerRow.value;
+                }
+            }
+
+            if (!originHeaderExists) {
+                requestDetails.requestHeaders.push(ruleToEnsure);
+            }
         }
 
         let result = {
             requestHeaders: requestDetails.requestHeaders
         };
 
-        this.sentFromUrl = this.tabObserver.getTabUrl(requestDetails.tabId);
-
         return result;
     }
 
     recieveResponse(responseDetails) {
-        let originHeaderExists = false;
-        let ruleToEnsure = {
-            name: headers.AccessControlAllowOrigin,
-            value: "*"
-        };
+        if (this.someRuleApply(this.sentFromUrl, responseDetails.url)) {
+            let originHeaderExists = false;
+            let ruleToEnsure = {
+                name: headers.AccessControlAllowOrigin,
+                value: "*"
+            };
 
-
-        for (let headerRow of responseDetails.responseHeaders) {
-            if (headerRow.name.toLowerCase() === ruleToEnsure.name.toLowerCase()) {
-                originHeaderExists = true;
-                headerRow.value = ruleToEnsure.value;
-                break;
+            for (let headerRow of responseDetails.responseHeaders) {
+                if (headerRow.name.toLowerCase() === ruleToEnsure.name.toLowerCase()) {
+                    originHeaderExists = true;
+                    headerRow.value = ruleToEnsure.value;
+                    break;
+                }
             }
+
+            if (!originHeaderExists) {
+                responseDetails.responseHeaders.push(ruleToEnsure);
+            }
+
+            if (this.accessControlPresent) {
+                responseDetails.responseHeaders.push({
+                    name: headers.AccessControlAllowHeaders,
+                    value: this.accessControlPresent
+                });
+            }
+
+            responseDetails.responseHeaders.push(headers.AccessControlAllowMethods);
         }
-
-        if (!originHeaderExists) {
-            responseDetails.responseHeaders.push(ruleToEnsure);
-        }
-
-
-        if (this.accessControlPresent) {
-            responseDetails.responseHeaders.push({
-                name: headers.AccessControlAllowHeaders,
-                value: this.accessControlPresent
-            });
-        }
-
-        responseDetails.responseHeaders.push(headers.AccessControlAllowMethods);
 
         let result = {
             responseHeaders: responseDetails.responseHeaders
         };
-
         this.resetRequestState();
 
         return result;
